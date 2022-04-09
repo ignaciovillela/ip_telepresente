cf29f7d1b57fbb666175fe76958f30e1f8722771
Ignacio Villela <ivillela@manager.cl>
2022-04-08 21:31:16 -0400

    update intranet

--- js/intranet.js
+++ js/intranet.js
@@ -1,10 +1,13 @@
 (function() {
     const form = document.querySelector('#form');
+    const submitButton = document.querySelector('#enviar');
     const email = document.querySelector('#email');
     const success = document.querySelector('#success');
     const formDiv = document.querySelector('#form-div');
     form.addEventListener('submit', (event) => {
         event.preventDefault();
+        submitButton.classList.add('disabled');
+        submitButton.setAttribute('disabled', 'disabled');
         setTimeout(() => {
             success.classList.remove('d-none');
             const user = email.value.replace('@telepresente.cl', '').replace('.', ' ');
